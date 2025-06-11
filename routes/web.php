bc33b3a861e181f545ff8b6f83524a878eaabe13
lussyanast <lussyanast@gmail.com>
2025-06-11 11:41:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\ReportController;
+use App\Http\Controllers\Admin\TestimonialController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PublicController::class, 'home']);
+Route::get('/catalog', [PublicController::class, 'catalog']);
+Route::get('/order', [PublicController::class, 'orderForm']);
+Route::post('/order', [PublicController::class, 'submitOrder']);
+Route::get('/gallery', [PublicController::class, 'gallery']);
+Route::get('/testimonials', [PublicController::class, 'testimonials']);
+Route::post('/testimonials', [PublicController::class, 'submitTestimonial']);
+Route::get('/contact', [PublicController::class, 'contactForm']);
+Route::post('/contact', [PublicController::class, 'submitContact']);
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::resource('/menus', MenuController::class);
+    Route::resource('/orders', OrderController::class);
+    Route::resource('/testimonials', TestimonialController::class);
+    Route::get('/reports', [ReportController::class, 'index']);
 });
